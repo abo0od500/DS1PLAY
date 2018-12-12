a9d877aea4e2d934c8fdcdd86d4ec4e88a81a443
Abdullah <alkhamsi.abdullah@gmail.com>
2018-12-12 17:01:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('guildMemberAdd', member => {
-let role = message.guild.roles.find(r => r.name === "Team Mystic");
- member.addRole(member.guild.roles.find(role => role.name === "HL")).catch(console.error);
+    member.addRole(member.guild.roles.find("name","HL")).catch(console.error);
 });
 
  
