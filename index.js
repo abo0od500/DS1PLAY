e47f495c351db7d1a21626a765150cded5d3c64d
Abdullah <alkhamsi.abdullah@gmail.com>
2019-03-06 08:19:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,11 +9,41 @@ var fs = require("fs");
 let mainChat = process.env.mainChat;
 //add Role and Welcomer
 
+
+client.on('ready', () => {
+    // "ready" isn't really ready. We need to wait a spell.
+    wait(1000);
+
+    // Load all invites for all guilds and save them to the cache.
+    client.guilds.forEach(g => {
+        g.fetchInvites().then(guildInvites => {
+            invites[g.id] = guildInvites;
+        });
+    });
+});
+
 client.on('guildMemberAdd', member => { 
 
-		let myRole = member.guild.roles.find(role => role.name === "Wiz");
-  member.addRole(myRole).catch(console.error);
+	let myRole = member.guild.roles.find(role => role.name === "Wiz");
+ 	 member.addRole(myRole).catch(console.error);
 	// client.channels.get(process.env.mainChat).send(`Welcome to Wiz Server -> ` + member.user.username);
+	
+	// Test GET invite code
+		// To compare, we need to load the current invite list.
+		member.guild.fetchInvites().then(guildInvites => {
+		// This is the *existing* invites for the guild.
+		const ei = invites[member.guild.id];
+
+		// Update the cached invites
+		invites[member.guild.id] = guildInvites;
+
+		// Look through the invites, find the one for which the uses went up.
+		const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+
+		console.log(invite.code);
+			
+			
+			
     var MASG1 = {username: member.user.username, avatarURL: member.user.avatarURL, createdAT: member.user.createdAt, id: member.user.id};
     request({
         method: 'POST',         
