88dd25a7c01bb6c99b45a89211aaaba91cc67c9a
Abdullah <alkhamsi.abdullah@gmail.com>
2019-03-06 14:38:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,16 +12,7 @@ let mainChat = process.env.mainChat;
 //add Role and Welcomer
 
 
-client.on('ready', () => {
-    // "ready" isn't really ready. We need to wait a spell.
-	wait(1000);
-    // Load all invites for all guilds and save them to the cache.
-    client.guilds.forEach(g => {
-        g.fetchInvites().then(guildInvites => {
-            invites[g.id] = guildInvites;
-        });
-    });
-});
+
 
 client.on('guildMemberAdd', member => { 
 
@@ -29,20 +20,7 @@ client.on('guildMemberAdd', member => {
  	 member.addRole(myRole).catch(console.error);
 	// client.channels.get(process.env.mainChat).send(`Welcome to Wiz Server -> ` + member.user.username);
 	
-	// Test GET invite code
-		// To compare, we need to load the current invite list.
-		member.guild.fetchInvites().then(guildInvites => {
-		// This is the *existing* invites for the guild.
-		const ei = invites[member.guild.id];
 
-		// Update the cached invites
-		invites[member.guild.id] = guildInvites;
-
-		// Look through the invites, find the one for which the uses went up.
-		const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-
-		console.log(invite.code);
-		 });
 			
 			
     var MASG1 = {username: member.user.username, avatarURL: member.user.avatarURL, createdAT: member.user.createdAt, id: member.user.id};
